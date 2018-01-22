b81bb7b2efe5e7f5b5ba5b5f1d785c70db5c59c6
wowenemy <wowenemy1488@gmail.com>
2018-01-22 21:08:14 +0200

    remuve
    remuve

--- test/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	
-		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		$('#zaebal').click(function(){
-		$(this).addClass('animated bounce');
-		$(this).one(animationEnd,function(){
-		});
-	}):
-});
